98c6688c054e5392222b7a3b541ce9139559148a
Kareem Golaub <kareemag@live.com>
2019-10-01 00:18:46 -0400

    Parser supports arithmetic, logical and comparison operators

--- a2/src/compiler488/parser/SyntaxErrorException.java
+++ a2/src/compiler488/parser/SyntaxErrorException.java
@@ -3,10 +3,10 @@ package compiler488.parser;
 /** Exception subclass for reporting parser syntax errors
  * @author Dave Wortman
  */
-public class SyntaxErrorException  extends Exception
+public class SyntaxErrorException extends Exception
 {
-  public  SyntaxErrorException( String msg )
+  public SyntaxErrorException(String msg)
   {
-    super( msg ) ;
+    super(msg) ;
   }
 }
