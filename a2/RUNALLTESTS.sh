8aa806485af7118fb5128f2aefdf44ae9937aed5
KyoKeun Park <kyokeunpark@gmail.com>
2019-10-06 18:22:52 -0400

    test directories are now traversable

--- a2/RUNALLTESTS.sh
+++ a2/RUNALLTESTS.sh
@@ -1,11 +1,34 @@
 #!/bin/sh
 
+# Global variable to hold the passed and failed tests per run
+declare -i PASSED=0
+declare -i FAILED=0
+
 # Prerequisite: argument 1 is a main directory where all the tests are
+#		argument 2 is the expected behaviour (fail or pass?). Either p or f.
 # Behaviour:	Runs all of the test within given directory, and returns
 #		number of failed tests and passed tests.
 function runtest() {
 	DIR=$1
-	declare -i PASSED=0
+
+	# Traverse through all of the sub-directories within given directory
+	for subdir in $DIR/*/
+	do
+		echo "$subdir"
+	done
 }
 
-ROOT="$(PWD)"
+# Prerequisite: This script must be run on the main directory of the A2 project.
+#		This is where "RUNALLTESTS.sh" and "RUNCOMPILER.sh" and "tests"
+#		directory should be located.
+
+ROOT="$(pwd)"
+
+# TODO: Uncomment once passing tests are merged
+# runtest $ROOT/tests/passing
+
+# Reset after the first run
+let PASSED=0
+let FAILED=0
+
+runtest $ROOT/tests/failing
