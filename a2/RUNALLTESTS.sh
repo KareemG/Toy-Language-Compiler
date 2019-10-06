9c248f14f94d53bf1ad1c0a9393fbcccf542a081
Kareem Golaub <kareemag@live.com>
2019-10-06 18:52:43 -0400

    Create test runner script

---
+++ a2/RUNALLTESTS.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+STARTDIR="$(pwd)"
+cd "$(dirname "$0")"
+BASEDIR="$(pwd)"
+cd "${STARTDIR}"
+
+
+for filename in ./tests/passing/**/*.488;
+do
+  echo -e "-> Parsing "$filename '\n'
+  java -jar "${BASEDIR}/dist/compiler488.jar" $filename
+  echo -e ""
+done
+
+for filename in ./tests/failing/**/*.488;
+do
+  echo -e "-> Parsing "$filename '\n'
+  java -jar "${BASEDIR}/dist/compiler488.jar" $filename
+  echo -e ""
+done
